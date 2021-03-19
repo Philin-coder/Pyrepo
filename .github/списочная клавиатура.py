a716bffb37094d28bb63167bc41c511c0be38cfe
Philin-coder <alexey.user.1988@gmail.com>
2021-03-20 00:11:50 +0300

    sorting

--- .github/списочная клавиатура.py
+++ .github/списочная клавиатура.py
@@ -1,6 +1,7 @@
 def listkey():
-    print(" ".join(map(str, [i ** 2 for i in map(int, input().split()) if i % 2 != 0 and (i ** 2) % 10 != 9])))
+    print(" ".join(map(str, [
+          i ** 2 for i in map(int, input().split()) if i % 2 != 0 and (i ** 2) % 10 != 9])))
+
+
 if __name__ == '__main__':
     listkey()
-    
-    

fa1e54b00f4b7720de175ba0d99a17e13b510a13
Philin-coder <alexey.user.1988@gmail.com>
2021-03-28 22:45:19 +0300

    ви т

--- .github/Морской Бой2.py
+++ .github/Морской Бой2.py
@@ -1,29 +1,61 @@
+#!/usr/bin/env python3
+ 
 class SeaMap(object):
-    
     def __init__(self):
         self.field = [ list('.')*10 for _ in range(10) ]
-   
+ 
     def shoot(self, row, col, result):
-        
-        def correct(x):
-            if x<0 :
-                x = 0
-            elif x>9:
-                x = 9
-            return x
-            
-        if result == 'sink':
-            line_row = [correct(x) for x in (row-1,row,row+1)]
-            line_col = [correct(x) for x in (col-1,col,col+1)]
-            
-            for i in line_row:
-                for x in line_col:
-                    self.field[i][x] = '*'
-            self.field[row][col] = 'x'
-        elif result == 'miss':
-            self.field[row][col] = '*'
-        else:   
-            self.field[row][col] = 'x'
+        self.field[row][col] = {
+            'miss': '*',
+            'hit': 'x',
+            'sink': 'x',
+        }[result]
  
     def cell(self, row, col):
-        return self.field[row][col]
+        f = self.field[row][col]
+        if f == 'x':
+            return 'x'
+        elif f == '*':
+            return '*'
+        elif f == '.':
+            return '*' if self._f(row, col) else '.'
+ 
+    def _f(self, x, y):
+        f1 = self._g(x-1, y-1)
+        f2 = self._g(x-1, y)
+        f3 = self._g(x-1, y+1)
+        f4 = self._g(x, y-1)
+        f5 = self._g(x, y+1)
+        f6 = self._g(x+1, y-1)
+        f7 = self._g(x+1, y)
+        f8 = self._g(x+1, y+1)
+        return 'x' in (f1, f2, f3, f4, f5, f6, f7, f8)
+ 
+    def _g(self, x, y):
+        try:
+            return self.field[x][y]
+        except IndexError:
+            return '.'
+ 
+ 
+ 
+sm = SeaMap()
+ 
+sm.shoot(0, 0, 'hit')
+sm.shoot(0, 1, 'sink')
+ 
+sm.shoot(9, 8, 'hit')
+sm.shoot(9, 9, 'sink')
+ 
+sm.shoot(2, 3, 'sink')
+ 
+sm.shoot(5, 6, 'miss')
+sm.shoot(7, 8, 'miss')
+sm.shoot(1, 7, 'miss')
+ 
+sm.shoot(1, 7, 'miss')
+ 
+for row in range(10):
+    for col in range(10):
+        print(sm.cell(row, col), end='')
+    print()

c4e7cbcfd30ebd89ac8058a3d2148a7e5ac4c389
Philin-coder <alexey.user.1988@gmail.com>
2021-04-01 22:52:08 +0300

    ghgg

---
+++ .github/pytest.py
@@ -0,0 +1,12 @@
+def findind(n):
+    mlist = list(map(int, input().split()))
+    print(mlist)
+    for i in mlist:
+        if i==n:
+            print(mlist.index(i))
+        
+        
+ 
+if __name__ == '__main__':
+    n = int(input())
+    findind(n)

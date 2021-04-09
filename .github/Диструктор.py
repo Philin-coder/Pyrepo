a46e32734268dc457a184b43d21d7a1322aec183
Philin-coder <alexey.user.1988@gmail.com>
2021-04-09 20:18:28 +0300

    efgh

---
+++ .github/Диструктор.py
@@ -0,0 +1,25 @@
+class A:
+    
+    def __init__(self, bb):
+
+        self.b = bb
+
+
+class B:
+
+    def __init__(self):
+
+        self.a = A(self)
+
+    def __del__(self):
+
+        print("die")
+
+
+def fun():
+    b = B()
+    
+
+
+if __name__ == '__main__':
+    fun()

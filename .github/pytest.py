7ff8daa843a8d1fa739772e9c04da82e1652169f
Philin-coder <alexey.user.1988@gmail.com>
2021-05-18 16:47:23 +0300

    оор\

--- .github/pytest.py
+++ .github/pytest.py
@@ -1,14 +1,5 @@
-import itertools
-import random
-from functools import reduce
-
-
-def magen(n):
-    mlist = list(random.randint(1, 10) for i in range(n))
-    print(*mlist)
-    print([i for i in mlist if i % 2 != 0])
-    print(reduce((lambda x, y: x*y), [i for i in mlist if i % 2 != 0]))
-
-
-if __name__ == '__main__':
-    magen(n=4)
+n = int(input())
+if n >= 1000 or n >= 1000:
+    print('подходит ')
+else:
+    print('нет')

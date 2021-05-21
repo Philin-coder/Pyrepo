48de9ee9cb9bfa6394afcca3820e52abe2ade9c3
Philin-coder <alexey.user.1988@gmail.com>
2021-05-21 20:49:02 +0300

    см

--- .github/второй максимум.py
+++ .github/второй максимум.py
@@ -1,13 +1,13 @@
 m1 = int(input())
-m2 = None
-while True:
+m2 = int(input())
+if m2 > m1:
+    m1, m2 = m2, m1
+while(True):
     a = int(input())
     if a == 0:
-        print(m2)
         break
-    elif a > m1:
+    if a > m1:
         m1, m2 = a, m1
-    elif m2 == None:
-        m2 = a
     elif a > m2:
         m2 = a
+print(m2)

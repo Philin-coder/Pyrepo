9d3429a69042501966a01950423128f47e3cfd27
Philin-coder <alexey.user.1988@gmail.com>
2021-05-21 20:49:48 +0300

    ш

--- .github/pytest.py
+++ .github/pytest.py
@@ -1,39 +1,10 @@
-import selectors
-import socket
-
-sel = selectors.DefaultSelector()
 
 
-def accept(sock, mask):
-    # Должен быть готов к чтению
-    conn, addr = sock.accept()
-    print('Создан сокет:', conn, 'для адреса:', addr)
-    # включаем для него неблокирующий режим
-    conn.setblocking(False)
-    # регистрируем для прослушивания событий клиента
-    sel.register(conn, selectors.EVENT_READ, read)
+a = int(input('первое число'))  # раз вводим число, жмем enter
+b = int(input('второе'))  # два вводим число, жмем enter
 
-
-def read(conn, mask):
-    # Должен быть готов к чтению
-    data = conn.recv(1000)
-    if data:
-        print('Отвечаем: ', repr(data), 'сокету =>', conn)
-        conn.send(data)
-    else:
-        print('Закрытие соединения', conn)
-        sel.unregister(conn)
-        conn.close()
-
-
-sock = socket.socket()
-sock.bind(('localhost', 8008))
-sock.listen(5)
-sock.setblocking(False)
-sel.register(sock, selectors.EVENT_READ, accept)
-
-while True:
-    events = sel.select()
-    for key, mask in events:
-        callback = key.data
-        callback(key.fileobj, mask)
+if a >= 0 and b >= 0:  # проверка
+    a, b = b, a
+    print('Переменные поменялись местами!', a, b)  # вывод рах
+else:
+    print('Введена не являющаяся числовым знач-ем переменная!')  # вывод два

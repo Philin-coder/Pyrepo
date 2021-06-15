251298197be6212410dd55d51f9a9be288917f6b
Philin-coder <alexey.user.1988@gmail.com>
2021-06-15 12:43:53 +0300

    wergthm,k

--- .github/pytest.py
+++ .github/pytest.py
@@ -1,2 +1,13 @@
-a = 4
-print(a, 'a')
+
+
+def masgen() -> list:
+    mlist = list(range(0, 49))
+    print(mlist)
+    for j, i in enumerate(mlist):
+        if i < 15:
+            mlist[j] = -1
+    print(mlist)
+
+
+if __name__ == '__main__':
+    print(masgen())

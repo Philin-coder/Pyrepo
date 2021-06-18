dddcfd88c933c0fd87f4a8fa0f8e3c502d299e59
Philin-coder <alexey.user.1988@gmail.com>
2021-06-18 17:28:13 +0300

    kp

--- .github/pytest.py
+++
@@ -1,9 +0,0 @@
-import csv
-from operator import itemgetter
-
-with open('studens.csv', encoding="utf-8", ) as file:
-    reader = csv.reader(file, delimiter=';')
-    students = list(reader)
-    del students[0]
-
-print(sorted(students, key=itemgetter(1)))

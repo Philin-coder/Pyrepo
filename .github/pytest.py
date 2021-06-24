fe80bc4627060584f8272da181974cf9870aa500
Philin-coder <alexey.user.1988@gmail.com>
2021-06-24 20:13:46 +0300

    hyuj

--- .github/pytest.py
+++ .github/pytest.py
@@ -1,7 +1,10 @@
-import numpy as np
-a = np.random.randint(1, 6, 10)
-print(a)
-tmp = a[::2].max()
-b = a[1::2]
-b[b == b.min()] = tmp
-print(a)
+base = {1:'I', 2:'II', 3:'III', 4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'IX', 10:'X',}
+str_from_file = '9'
+if str_from_file.isdigit():
+    digit = int(str_from_file)
+    if 0 < digit < 11:
+        print (base[digit])
+    else:
+        print('Число вне диапазона')
+else:
+    print('Это не число')

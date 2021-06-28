182271e31b1f53972b70086b52510692bfb75281
Philin-coder <alexey.user.1988@gmail.com>
2021-06-28 22:27:42 +0300

    kkk

--- .github/pytest.py
+++ .github/pytest.py
@@ -1,6 +1,2 @@
-def task(mystr: str) -> str:
-    return f'{"Потому, что главдиолусы"}'
-
-
-if __name__ == '__main__':
-    print(task(mystr=input("Почему же автосервис?")))
+import sqlalchemy
+print(sqlalchemy.__version__)

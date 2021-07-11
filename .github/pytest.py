f9fd7875ee20990f3d6f098b7f7bf4ba865ba87f
Philin-coder <alexey.user.1988@gmail.com>
2021-07-12 00:08:24 +0300

    lop

--- .github/pytest.py
+++ .github/pytest.py
@@ -1,11 +1,4 @@
-from bisect import insort
- 
-def insertionSorted(iterable, steps=3):
-    it = iter(iterable)
-    result = []
-    for _, x in zip(range(steps + 1), it):
-        insort(result, x)
-    result.extend(it)
-    return result
-    
-assert insertionSorted((8, 1, 7, 4, 3, 9, 2, 5, 6, 10)) == [1, 4, 7, 8, 3, 9, 2, 5, 6, 10]
+import math
+lst = [5, 5.16, 20, 32.21, 41.45]  # кто ж числа в кавычках пишет?
+for i in lst:
+    print(math.modf(i))

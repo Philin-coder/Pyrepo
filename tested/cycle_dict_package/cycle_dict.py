68a70d4b1effe56fa7e8528cb30337cdafd14c39
Philin-coder <alexey.user.1988@gmail.com>
2021-09-20 17:05:28 +0300

    add:dictgen with cycle

---
+++ tested/cycle_dict_package/cycle_dict.py
@@ -0,0 +1,12 @@
+def cycle_dict_func(key_list: list) -> dict:
+    """
+    ПРИмер циклически созданого словаря
+    :param key_list: список ключей словаря
+    :return: словарь
+     cycle_dict_func(key_list=['раз', 'два', 'Три'])
+    {'раз': 1, 'два': 2, 'Три': 3}
+    """
+    if isinstance(key_list, list):
+        return dict(zip(key_list, range(1, 1 + len(key_list))))
+    else:
+        raise TypeError('Передане не списсок')

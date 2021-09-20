c62fae1df00cedb7942b3d090cec438c42496178
Philin-coder <alexey.user.1988@gmail.com>
2021-09-20 20:33:25 +0300

    add:dicmerger

--- tested/dict_in_class_package/dict_in_class.py
+++ tested/dict_in_class_package/dict_in_class.py
@@ -4,6 +4,10 @@ class Employee(object):
     >>> man = Employee('john', 222, 222.2, 2, 12.2)
     >>> man.__dict__
     {'name': 'john', 'nom': 222, 'zp': 222.2, 'u': 2, 'kom': 12.2}
+    >>> man = Employee('john', 222, 222.2, 2, 12.2)
+    >>> print(man.__str__())
+    ('Employee:john', 'number 222', 'pay222.2', 'restrict2', 'kom12.2')
+
     """
 
     def __init__(self, name, nom, zp, u, kom):
@@ -15,8 +19,3 @@ class Employee(object):
 
     def __str__(self):
         return f'Employee:{self.name}', f'number {self.nom}', f'pay{self.zp}', f'restrict{self.u}', f'kom{self.kom}'
-
-if __name__ == '__main__':
-    man = Employee('john', 222, 222.2, 2, 12.2)
-    print(man.__dict__)
-    print(man.__str__())

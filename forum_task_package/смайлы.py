c0ab150539c2f0cba3ecc9c72ac819ec0f6b7e19
Philin-coder <alexey.user.1988@gmail.com>
2021-09-21 22:41:47 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- forum_task_package/смайлы.py
+++ forum_task_package/смайлы.py
@@ -1,9 +1,9 @@
 # TODO: and me
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 
-source_img = Image.open(file_name).convert("RGBA")
+source_img = Image.open('file_name').convert("RGBA")
 
 draw = ImageDraw.Draw(source_img)
 draw.rectangle(((0, 0), (100, 100)), fill="black")
 
-source_img.save(out_file, "JPEG")
+source_img.save('out_file', "JPEG")

98b67f7feeed722d74e76d9627d6fcc0c506cf58
Philin-coder <alexey.user.1988@gmail.com>
2021-09-21 22:18:49 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- forum_task_package/several/pygame музыка.py
+++ forum_task_package/several/pygame музыка.py
@@ -1,21 +1,24 @@
 import os
-import  pygame
-fext = 'mp3'
+import sys
+import pygame
+
+play_list = list()
+f_ext = 'mp3'
 for i in os.listdir(os.getcwd()):
-    if i.endswith(fext):
-        playlist.append(i)
-        print('список песен равен')
-        print(playlist)
-        for i in range(len(playlist)):
-            pygame.init()
-            wnd = pygame.display.set_mode((640, 640))
-            pygame.mixer.music.load(playlist[i])
-            pygame.mixer.music.play(-1, 0.0)
-            circule = pygame.draw.circle(wnd, (50, 30, 90), (90, 30), 16, 5)
-            wnd.blit(wnd, circule)
-            while True:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        sys.exit()
-                pygame.display.update()
+    if i.endswith(f_ext):
+        play_list.append(i)
+        print(f'список песен равен')
+        # print(play_list)
+        # for i in range(len(play_list)):
+        #     pygame.init()
+        #     wnd = pygame.display.set_mode((640, 640))
+        #     pygame.mixer.music.load(play_list[i])
+        #     pygame.mixer.music.play(-1, 0.0)
+        #     mc_ = pygame.draw.circle(wnd, (50, 30, 90), (90, 30), 16, 5)
+        #     wnd.blit(wnd, mc)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            # pygame.display.update()

cff106838dc658b00b9fa6c667beb006c959e0c4
Philin-coder <alexey.user.1988@gmail.com>
2021-09-22 00:08:41 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- forum_task_package/several/vcf1.py
+++ forum_task_package/several/vcf1.py
@@ -1,20 +1,23 @@
-def get(self, href):
-    """Fetch a single item."""
-    if self.is_fake:
-        return
-
-    uid = _trim_suffix(href, ('.ics', '.ical', '.vcf'))
-    etesync_item = self.collection.get(uid)
-    if etesync_item is None:
-        return None
-
-    try:
-        item = vobject.readOne(etesync_item.content)
-    except Exception as e:
-        raise RuntimeError("Failed to parse item %r in %r" %
-                           (href, self.path)) from e
-
-    last_modified = time.strftime(
-        "%a, %d %b %Y %H:%M:%S GMT",
-        time.gmtime(time.time()))
-    return EteSyncItem(self, item, href, last_modified=last_modified, etesync_item=etesync_item)
+# import time
+#
+#
+# def get(self, href):
+#     """Fetch a single item."""
+#     if self.is_fake:
+#         return
+#
+#     uid = a_trim_suffix(href, ('.ics', '.i_cal', '.vcf'))
+#     ete_sync_item = self.collection.get(uid)
+#     if ete_sync_item is None:
+#         return None
+#
+#     try:
+#         item = v_object.readOne(ete_sync_item.content)
+#     except Exception as e:
+#         raise RuntimeError("Failed to parse item %r in %r" %
+#                            (href, self.path)) from e
+#
+#     last_modified = time.s_trf_time(
+#         "%a, %d %b %Y %H:%M:%S GMT",
+#         time.gm_time(time.time()))
+#     return e_te_Sync_Item(self, item, href, last_modified=last_modified, et_es_ync_item=ete_sync_item)

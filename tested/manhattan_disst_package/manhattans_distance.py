0476c147f53b4c4c65c26f7bd9b4f74362222f08
Philin-coder <alexey.user.1988@gmail.com>
2021-09-22 18:01:57 +0300

    Merge remote-tracking branch 'pyrepo/master'

---
+++ tested/manhattan_disst_package/manhattans_distance.py
@@ -0,0 +1,20 @@
+def manhattan_distance_func(v1: list, v2: list) -> [float, int]:
+    """
+    Манхетенское  расстояние между объектами
+    :param v1: списоок координат первого объекта
+    :param v2: списоок координат второго  объекта
+    :return:Манхетенское  расстояние между объектами
+    >>> manhattan_distance_func(v1=[12.0, 11.0, 2], v2=[11.0, 9.0, 1.0])
+    4.0
+    >>> manhattan_distance_func(v1=[12.0], v2=[11.0])
+    1.0
+    >>> manhattan_distance_func(v1=[12.0], v2=[11.0, 1112, 4])
+    1.0
+
+    """
+    if isinstance(v1, list) and isinstance(v2, list) and not any(isinstance(i, str) for i in v1) and not any(
+            isinstance(i, str) for i in v2):
+        return sum((x - y) for (x, y) in zip(v1, v2))
+
+    else:
+        raise TypeError('передан неверный тип данных')

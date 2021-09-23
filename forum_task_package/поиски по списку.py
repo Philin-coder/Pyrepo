e8f3fdea706fa1632a24f5051cf057783b5b7be7
Philin-coder <alexey.user.1988@gmail.com>
2021-09-24 00:14:06 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- forum_task_package/поиски по списку.py
+++
@@ -1,8 +0,0 @@
-def filtren():
-    mixed = ['мак', 'просо', 'мак', 'мак', 'просо', 'мак', 'просо', 'просо', 'просо', 'мак']
-    zolushka = list(filter(lambda x: x == 'мак', mixed))
-    print(zolushka)
-
-
-if __name__ == '__main__':
-    filtren()

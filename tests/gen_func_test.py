74e689d81b4ac598cedd29b7aaa85a8c8906ee1a
Philin-coder <alexey.user.1988@gmail.com>
2021-09-23 12:55:00 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- tests/gen_func_test.py
+++ tests/gen_func_test.py
@@ -9,7 +9,7 @@ def load_tests(loader, tests, ignore):
     return tests
 
 
-class GenfFncTest(TestCase):
+class GenFncTest(TestCase):
     def test_gen_works_lot_of(self):
         self.assertEqual(gen_start(a=0, b=1, mx=4000000), 4613732)
 
@@ -26,14 +26,16 @@ class GenfFncTest(TestCase):
         with self.assertRaises(TypeError) as e:
             gen_start(a=-10, b=-10, mx=10)
         self.assertEqual(
-            'Введите положительные целые числа для шага генератора, максимума, генератора и начального значения генератора',
+            'Введите положительные целые числа для шага генератора, максимума, генератора и '
+            'начального значения генератора',
             e.exception.args[0])
 
     def test_gen_chars(self):
         with self.assertRaises(TypeError) as e:
             gen_start(a='10', b=-10, mx=10)
         self.assertEqual(
-            'Введите положительные целые числа для шага генератора, максимума, генератора и начального значения генератора',
+            'Введите положительные целые числа для шага генератора, максимума, генератора'
+            ' и начального значения генератора',
             e.exception.args[0])
 
 

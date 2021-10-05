d9509ec6a3788a2ad7e8ffa25adeedb034f3a013
Philin-coder <alexey.user.1988@gmail.com>
2021-10-06 00:09:41 +0300

    Merge remote-tracking branch 'pyrepo/master'

---
+++ tests/half_list_test.py
@@ -0,0 +1,23 @@
+from unittest import TestCase, main
+from tested.half_list_package import half_lister
+from tested.half_list_package.half_lister import half_list_gen_func
+import doctest
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(half_lister))
+    return tests
+
+
+class HalfTest(TestCase):
+    def test_int_input(self):
+        self.assertEqual(half_list_gen_func(n=10), ([6, 7, 8, 9, 10], [1, 2, 3, 4, 5]))
+
+    def test_wrong_input(self):
+        with self.assertRaises(TypeError) as e:
+            half_list_gen_func(n='10')
+        self.assertEqual('Введите целое число', e.exception.args[0])
+
+
+if __name__ == '__main__':
+    main()

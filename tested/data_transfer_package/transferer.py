f519e1ab5e6c296d894ffa4b41f2f90b742aa6e9
Philin-coder <alexey.user.1988@gmail.com>
2021-10-31 14:31:30 +0300

    Merge remote-tracking branch 'pyrepo/master'

---
+++ tested/data_transfer_package/transferer.py
@@ -0,0 +1,28 @@
+import random
+
+
+def create_matrix(n: int) -> list:
+    """
+    Генерация матрицы случайных чисел.
+    :param n:Диапазон.
+    :return: список чисел в диапазоне
+    """
+    if isinstance(n, int) and n > 0 and n != 0:
+        return [[random.randint(0, 9) for _ in range(n)] for _ in range(n)]
+    else:
+        raise TypeError('Введите число')
+
+
+def show_matrix(matrix: list):
+    """
+    Вывод списка случайных чисел в виде матрицы
+    :param matrix:
+    :return: Матрица случайных чисел.
+    """
+    if matrix is not None:
+        for i in matrix:
+            for j in i:
+                print(j, end=' ')
+            print()
+    else:
+        raise TypeError('Для вывода передан не список')

a33a505ae714c2174bb7ad851a4f7fd79bdebca2
Philin-coder <alexey.user.1988@gmail.com>
2021-11-02 00:05:55 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- tested/class_poperty_and_method_package/class_meh.py
+++ tested/class_poperty_and_method_package/class_meh.py
@@ -25,15 +25,3 @@ class Stud(object):
             return "%s" % (self.name + ' ' + self.nickname)
         else:
             raise TypeError('Передан неверный тип данных')
-
-
-if __name__ == '__main__':
-    st = Stud(name=1, nickname='Ger Petter')
-    print(Stud.name_mul_three(st.name))
-    print(st.nick_name_generator)
-
-    st = Stud(name=1, nickname='Ger Petter')
-    # st = Stud(name='1', nickname='Ger Petter')
-    sl = Stud(name='Павел ', nickname='Apostol')
-    print(Stud.name_mul_three(sl.name))
-    print(sl.nick_name_generator)

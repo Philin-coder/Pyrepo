051e5f7bbe0170170b34d73d8855ebc49af81cf0
Philin-coder <alexey.user.1988@gmail.com>
2021-11-05 16:00:12 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- forum_task_package/laba fon/1.py
+++ forum_task_package/laba fon/1.py
@@ -1,22 +1,19 @@
 from tkinter import *
-import socket
-import os
 from PIL import Image, ImageTk
 from time import sleep
-import pygame
 
 
-class app(Frame):
+class MyApp(Frame):
     root = Tk()
     root.geometry("640x480")
 
     img = Image.open("1.jpg")
     render = ImageTk.PhotoImage(img)
     root.title("test")
-    initil = Label(root, image=render)
-    initil.image = render
-    initil.pack()
+    init_l = Label(root, image=render)
+    init_l.image = render
+    init_l.pack()
     root.mainloop()
     sleep(3)
     root.quit()
-    run = app()
+    run = MyApp()

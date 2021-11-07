925014d0e8357e61ff2d43b0fffaa459ccaea6c4
Philin-coder <alexey.user.1988@gmail.com>
2021-11-07 18:28:57 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- forum_task_package/регулярки.py
+++ forum_task_package/регулярки.py
@@ -5,7 +5,7 @@ def reader(my_filename, fext2):
     with open(my_filename + '.' + fext2, 'r', encoding='utf-8') as fp:
         data = fp.readlines()
         data = str(data)
-        # res = re.findall(r'.', data)
+        res = re.findall(r'.', data)
         # res=re.findall(r'\w',data)
         # res=re.findall(r'\w*',data)
         # res=re.findall(r'\w+',data)

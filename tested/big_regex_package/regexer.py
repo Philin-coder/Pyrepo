19ca1b9dbf0463dedfc0529748a4203e571c823a
Philin-coder <alexey.user.1988@gmail.com>
2021-11-08 12:27:09 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- tested/big_regex_package/regexer.py
+++ tested/big_regex_package/regexer.py
@@ -43,9 +43,9 @@ def one_sym(my_filename: str, f_ext: str) -> list:
     :param f_ext: расширение.
     :return: Результат извлечения из текста в файле всех символов.
     Образец вызова.
-    one_sym(my_filename='test', f_ext='txt')
-   ['[', "'", 'A', 'V', ' ', 'i', 's', ' ', 'l', 'a', 'r', 'g', 'e', 's', 't', ' ', 'A', 'n', 'a',
-   'l', 'y', 't', 'i', 'c', 's', ' ', 'c', 'o', 'm', 'm', 'u', 'n', 'i', 't', 'y', ' ', 'o', 'f', ' ', 'I', 'n', 'd',
+    one_sym(my_filename='r_test', f_ext='txt')
+   ['[', "'", 'A', 'V',' ', 'i', 's',' ', 'l', 'a', 'r', 'g', 'e', 's', 't',' ', 'A', 'n', 'a',
+   'l', 'y', 't', 'i', 'c', 's',' ', 'c', 'o', 'm', 'm', 'u', 'n', 'i', 't', 'y',' ', 'o', 'f',' ', 'I', 'n', 'd',
    'i', 'a', '\\', 'n', "'", ']']
     """
     if os.path.isfile(my_filename + '.' + f_ext):
@@ -83,7 +83,8 @@ def letters_with_spaces(my_filename: str, f_ext: str) -> list:
     :return:  Результат извлечения из текста в файле всех букв м пробелов
     Образец вызова.
     letters_with_spaces(my_filename='test', f_ext='txt')
-    ['', '', 'AV', '', 'is', '', 'largest', '', 'Analytics', '', 'community', '', 'of', '', 'India', '', 'n', '', '', '']
+    ['', '', 'AV', '', 'is', '', 'largest', '', 'Analytics', '', 'community', '', 'of', '', '
+    India', '', 'n', '', '', '']
     """
     if os.path.isfile(my_filename + '.' + f_ext):
         with open(my_filename + '.' + f_ext, 'r', encoding='utf-8') as fp:
@@ -112,6 +113,13 @@ def letters_only(my_filename: str, f_ext: str) -> list:
 
 
 def first_word_only(text_str: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param text_str: исходная строкаа.
+    :return: Результат обработки(поиска первого слова строки).
+    >>> first_word_only(text_str='AV is largest Analytics community of India')
+    ['AV']
+    """
     if isinstance(text_str, str) and text_str != '':
         return re.findall(r'^\w+', text_str)
     else:
@@ -119,6 +127,13 @@ def first_word_only(text_str: str) -> list:
 
 
 def last_word_only(text_str: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param text_str: Исходный строка
+    :return: Результат обработки(поиска последнего  слова строки)
+    >>> last_word_only(text_str='AV is largest Analytics community of India')
+    ['India']
+    """
     if isinstance(text_str, str) and text_str != '':
         return re.findall(r'\w+$', text_str)
     else:
@@ -126,6 +141,15 @@ def last_word_only(text_str: str) -> list:
 
 
 def two_letters_from_begin_no_spaces(my_filename: str, f_ext: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param my_filename: Имя файла
+    :param f_ext: Расширение.
+    :return: результат извлечения из файла двух первых букв каждого слова.
+    Образец вызова.
+    two_letters_from_begin_no_spaces(my_filename='r_test', f_ext='txt')
+    ['AV', 'is', 'la', 'rg', 'es', 'An', 'al', 'yt', 'ic', 'co', 'mm', 'un', 'it', 'of', 'In', 'di']
+    """
     if os.path.isfile(my_filename + '.' + f_ext):
         with open(my_filename + '.' + f_ext, 'r', encoding='utf-8') as fp:
             data = str(fp.readlines())
@@ -135,6 +159,15 @@ def two_letters_from_begin_no_spaces(my_filename: str, f_ext: str) -> list:
 
 
 def two_letters_from_begin_no_spaces_as_word(my_filename: str, f_ext: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param my_filename:имя файла.
+    :param f_ext: Расширение.
+    :return: результат извлечения из файла двух первых букв каждого слова(границы слова).
+    Образец вызова.
+    two_letters_from_begin_no_spaces_as_world(my_filename='r_test', f_ext='txt')
+    ['AV', 'is', 'la', 'An', 'co', 'of', 'In', "n'"]
+    """
     if os.path.isfile(my_filename + '.' + f_ext):
         with open(my_filename + '.' + f_ext, 'r', encoding='utf-8') as fp:
             data = str(fp.readlines())
@@ -144,6 +177,13 @@ def two_letters_from_begin_no_spaces_as_word(my_filename: str, f_ext: str) -> li
 
 
 def dom_finder(text_str: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param text_str: исходная строка.
+    :return: домены ииз этой строки.
+    >>> dom_finder(text_str='abc.test@gmail.com, xyz@test.in, test.first@analyticsvidhya.com, first.test@rest.biz')
+    ['com', 'in', 'com', 'biz']
+    """
     if isinstance(text_str, str) and text_str != '':
         return re.findall(r'@\w+.(\w+)', text_str)
     else:
@@ -151,6 +191,13 @@ def dom_finder(text_str: str) -> list:
 
 
 def date_finder(text_str: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param text_str: исходная строка.
+    :return: результат обработки(поиск даты).
+    >>> date_finder(text_str='Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009')
+    ['12-05-2007', '11-11-2011', '12-01-2009']
+    """
     if isinstance(text_str, str) and text_str != '':
         return re.findall(r'\d{2}-\d{2}-\d{4}', text_str)
     else:
@@ -158,6 +205,13 @@ def date_finder(text_str: str) -> list:
 
 
 def year_finder(text_str: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param text_str: Исходная строка.
+    :return: поиск года в ней.
+    >>> year_finder(text_str='Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009')
+    ['2007', '2011', '2009']
+    """
     if isinstance(text_str, str) and text_str != '':
         return re.findall(r'\d{2}-\d{2}-(\d{4})', text_str)
     else:
@@ -165,6 +219,13 @@ def year_finder(text_str: str) -> list:
 
 
 def vols_in_begin(text_str: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param text_str: исходная строка.
+    :return: Результат обработки(слова, начинающиеся на гласную)
+    >>> vols_in_begin(text_str='Task, is , very very, difficult to Me')
+    ['is']
+    """
     if isinstance(text_str, str) and text_str != '':
         return re.findall(r'\b[aeiouAEIOU]\w+', text_str)
     else:
@@ -172,6 +233,13 @@ def vols_in_begin(text_str: str) -> list:
 
 
 def invert_search(text_str: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param text_str: исходная строка.
+    :return: Результат обработки(Инверсия поиска).
+    >>> invert_search(text_str='Task, is , very very, difficult to Me')
+    ['Task', 'very', ' very', 'difficult', ' to', ' Me']
+    """
     if isinstance(text_str, str) and text_str != '':
         return re.findall(r'\b[^aeiouAEIOU]\w+', text_str)
     else:
@@ -179,6 +247,13 @@ def invert_search(text_str: str) -> list:
 
 
 def invert_search_with_space(text_str: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param text_str: исходная строка.
+    :return: Результат обработки(Инверсия поиска с учетом пробелов).
+    >>> invert_search_with_space(text_str='AV is largest Analytics community of India')
+    ['largest', 'community']
+    """
     if isinstance(text_str, str) and text_str != '':
         return re.findall(r'\b[^aeiouAEIOU ]\w+', text_str)
     else:
@@ -186,6 +261,17 @@ def invert_search_with_space(text_str: str) -> list:
 
 
 def num_checker(nums: list) -> str:
+    """
+    Работа со списком.
+    :param nums: исходный список.
+    :return: является ли содержимое списка телефонным номером.
+    >>> num_checker(nums=['9999999999', '999999-999', '99999x9999'])
+    'yes'
+    >>> num_checker(nums=['999999-999', '99999x9999'])
+    'no'
+    >>> num_checker(nums=['999999-999'])
+    'no'
+    """
     if isinstance(nums, list) and nums != []:
         for i in nums:
             if re.match(r'[8-9][0-9]{9}', i) and len(i) == 10:
@@ -197,6 +283,13 @@ def num_checker(nums: list) -> str:
 
 
 def str_cleaner(text_str: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param text_str: исходная строка.
+    :return: Результат обработки(убраны все разделители).
+    >>> str_cleaner(text_str='no more;fake,snd,lie,for me')
+    ['no', 'more', 'fake', 'snd', 'lie', 'for', 'me']
+    """
     if isinstance(text_str, str) and text_str != '':
         return re.split(r'[;,\s]', text_str)
     else:
@@ -204,6 +297,13 @@ def str_cleaner(text_str: str) -> list:
 
 
 def str_cleaner_spaces(text_str: str) -> str:
+    """
+    Обработка строки регулярными выражениями.
+    :param text_str: исходная строка.
+    :return: Результат обработки(убраны все разделители, включая пробелы).
+    >>> str_cleaner_spaces(text_str='no more;fake,snd,lie,for me')
+    'no more fake snd lie for me'
+    """
     if isinstance(text_str, str) and text_str != '':
         return re.sub(r'[;,\s]', ' ', text_str)
     else:
@@ -211,72 +311,14 @@ def str_cleaner_spaces(text_str: str) -> str:
 
 
 def html_cleaner(text_str: str) -> list:
+    """
+    Обработка строки регулярными выражениями.
+    :param text_str: исходная строка.
+    :return:Получение Текста.
+    >>> html_cleaner(text_str='1NoahEmma2LiamOlivia3MasonSophia4JacobIsabella5WilliamAva6EthanMia7MichaelEmily')
+    [('Noah', 'Emma'), ('Liam', 'Olivia'), ('Mason', 'Sophia'), ('Jacob', 'Isabella'), ('William', 'Ava'), ('Ethan', 'Mia'), ('Michael', 'Emily')]
+    """
     if isinstance(text_str, str) and text_str != '':
         return re.findall(r'\d([A-Z][A-Za-z]+)([A-Z][A-Za-z]+)', text_str)
     else:
         raise TypeError('Передан неверный тип данных')
-
-
-if __name__ == '__main__':
-    print(cont_gen(file_cont='AV is largest Analytics community of India'))
-    # print(cont_gen(file_cont={'AV is  largest Analytics community of India.'}))
-
-    print(write_to_file(file_name='r_test', f_ext='txt',
-                        fdata=cont_gen(file_cont='AV is largest Analytics community of India')))
-
-    # print(write_to_file(file_name='r_test', f_ext='tt',
-    #                     fdata=cont_gen(file_cont='AV is largest Analytics community of India')))
-
-    print(one_sym(my_filename='r_test', f_ext='txt'))
-    # print(one_sym(my_filename='r_test', f_ext='tt'))
-
-    print(one_letter(my_filename='r_test', f_ext='txt'))
-    # print(one_letter(my_filename='r_test', f_ext='tt'))
-
-    print(letters_with_spaces(my_filename='r_test', f_ext='txt'))
-    # print(letters_with_spaces(my_filename='r_test', f_ext='tt'))
-
-    print(letters_only(my_filename='r_test', f_ext='txt'))
-    # print(letters_only(my_filename='r_test', f_ext='tt'))
-
-    print(first_word_only(text_str='AV is largest Analytics community of India'))
-    # print(first_word_only(text_str=123))
-
-    print(last_word_only(text_str='AV is largest Analytics community of India'))
-    # print(last_word_only(text_str=123))
-
-    print(two_letters_from_begin_no_spaces(my_filename='r_test', f_ext='txt'))
-    # print(two_letters_from_begin_no_spaces(my_filename='r_test', f_ext='tt'))
-
-    print(two_letters_from_begin_no_spaces_as_word(my_filename='r_test', f_ext='txt'))
-    # print(two_letters_from_begin_no_spaces_as_word(my_filename='r_test', f_ext='tt'))
-
-    print(dom_finder(text_str='abc.test@gmail.com, xyz@test.in, test.first@analyticsvidhya.com, first.test@rest.biz'))
-    # print(dom_finder(text_str=123))
-
-    print(date_finder(text_str='Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009'))
-    # print(date_finder(text_str=''))
-
-    print(year_finder(text_str='Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009'))
-    # print(year_finder(text_str=''))
-
-    print(vols_in_begin(text_str='Task, is , very very, difficult to Me'))
-    # print(vols_in_begin(text_str=''))
-
-    print(invert_search(text_str='Task, is , very very, difficult to Me'))
-    # print(invert_search(text_str=''))
-
-    print(invert_search_with_space(text_str='AV is largest Analytics community of India'))
-    # print(invert_search_with_space(text_str=''))
-    print(num_checker(nums=['9999999999', '999999-999', '99999x9999']))
-    print(num_checker(nums=['999999-999', '99999x9999']))
-    print(num_checker(nums=['999999-999']))
-    # print(num_checker(nums=[]))
-    print(str_cleaner(text_str='no more;fake,snd,lie,for me'))
-    # print(str_cleaner(text_str=''))
-
-    print(str_cleaner_spaces(text_str='no more;fake,snd,lie,for me'))
-    # print(str_cleaner_spaces(text_str=''))
-
-    print(html_cleaner(text_str='1NoahEmma2LiamOlivia3MasonSophia4JacobIsabella5WilliamAva6EthanMia7MichaelEmily'))
-    # print(html_cleaner(text_str=''))
--- tests/big_regex_test.py
+++ tests/big_regex_test.py
@@ -1,13 +1,25 @@
 from unittest import TestCase, main
-from big_regex_package import regexer
-from big_regex_package.regexer import cont_gen
-from big_regex_package.regexer import write_to_file
-from big_regex_package.regexer import one_sym
-from big_regex_package.regexer import one_letter
-from big_regex_package.regexer import letters_with_spaces
-from big_regex_package.regexer import letters_only
-from big_regex_package.regexer import first_word_only
-
+from tested.big_regex_package import regexer
+from tested.big_regex_package.regexer import cont_gen
+from tested.big_regex_package.regexer import write_to_file
+from tested.big_regex_package.regexer import one_sym
+from tested.big_regex_package.regexer import one_letter
+from tested.big_regex_package.regexer import letters_with_spaces
+from tested.big_regex_package.regexer import letters_only
+from tested.big_regex_package.regexer import first_word_only
+from tested.big_regex_package.regexer import last_word_only
+from tested.big_regex_package.regexer import two_letters_from_begin_no_spaces
+from tested.big_regex_package.regexer import two_letters_from_begin_no_spaces_as_word
+from tested.big_regex_package.regexer import dom_finder
+from tested.big_regex_package.regexer import date_finder
+from tested.big_regex_package.regexer import year_finder
+from tested.big_regex_package.regexer import vols_in_begin
+from tested.big_regex_package.regexer import invert_search
+from tested.big_regex_package.regexer import invert_search_with_space
+from tested.big_regex_package.regexer import num_checker
+from tested.big_regex_package.regexer import str_cleaner
+from tested.big_regex_package.regexer import str_cleaner_spaces
+from tested.big_regex_package.regexer import html_cleaner
 import doctest
 
 
@@ -76,6 +88,123 @@ class BigRegexTest(TestCase):
         with self.assertRaises(FileNotFoundError) as e:
             letters_only(my_filename='r_test', f_ext='tt')
         self.assertEqual('Файл имеет неверный тип ', e.exception.args[0])
+
+    def test_first_word_only_right(self):
+        self.assertEqual(first_word_only(text_str='AV is largest Analytics community of India'), ['AV'])
+
+    def test_first_word_only_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            first_word_only(text_str=123)
+        self.assertEqual('Передана не строка, или-строка пуста', e.exception.args[0])
+
+    def test_last_word_only_right(self):
+        self.assertEqual(last_word_only(text_str='AV is largest Analytics community of India'), ['India'])
+
+    def test_last_word_only_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            last_word_only(text_str=123)
+        self.assertEqual('Передана не строка, или-строка пуста', e.exception.args[0])
+
+    def test_two_letters_from_begin_no_spaces(self):
+        self.assertEqual(two_letters_from_begin_no_spaces(my_filename='r_test', f_ext='txt'),
+                         ['AV', 'is', 'la', 'rg', 'es', 'An', 'al', 'yt', 'ic', 'co', 'mm', 'un', 'it', 'of', 'In',
+                          'di'])
+
+    def test_two_letters_from_begin_no_spaces_as_word_right(self):
+        self.assertEqual(two_letters_from_begin_no_spaces_as_word(my_filename='r_test', f_ext='txt'),
+                         ['AV', 'is', 'la', 'An', 'co', 'of', 'In', "n'"])
+
+    def test_dom_finder_right(self):
+        self.assertEqual(
+            dom_finder(text_str='abc.test@gmail.com, xyz@test.in, test.first@analyticsvidhya.com, first.test@rest.biz'),
+            ['com', 'in', 'com', 'biz'])
+
+    def test_dom_finder_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            dom_finder(text_str=123)
+        self.assertEqual('Передана не строка, или-строка пуста', e.exception.args[0])
+
+    def test_date_finder_right(self):
+        self.assertEqual(
+            date_finder(text_str='Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009'),
+            ['12-05-2007', '11-11-2011', '12-01-2009'])
+
+    def test_date_finder_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            date_finder(text_str='')
+        self.assertEqual('Передана не строка, или-строка пуста', e.exception.args[0])
+
+    def test_year_finder_right(self):
+        self.assertEqual(
+            year_finder(text_str='Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009'),
+            ['2007', '2011', '2009'])
+
+    def test_year_finder_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            year_finder(text_str='')
+        self.assertEqual('Передана не строка, или-строка пуста', e.exception.args[0])
+
+    def test_vols_in_begin_right(self):
+        self.assertEqual(vols_in_begin(text_str='Task, is , very very, difficult to Me'), ['is'])
+
+    def test_vols_in_begin_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            vols_in_begin(text_str='')
+        self.assertEqual('Передана не строка, или-строка пуста', e.exception.args[0])
+
+    def test_invert_search_right(self):
+        self.assertEqual(invert_search(text_str='Task, is , very very, difficult to Me'),
+                         ['Task', 'very', ' very', 'difficult', ' to', ' Me'])
+
+    def test_invert_search_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            invert_search(text_str='')
+        self.assertEqual('Передана не строка, или-строка пуста', e.exception.args[0])
+
+    def test_invert_search_with_spaces_right(self):
+        self.assertEqual(invert_search_with_space(text_str='AV is largest Analytics community of India'),
+                         ['largest', 'community'])
+
+    def test_invert_search_with_spaces_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            invert_search_with_space(text_str='')
+        self.assertEqual('Передана не строка, или-строка пуста', e.exception.args[0])
+
+    def test_num_checker_right(self):
+        self.assertEqual(num_checker(nums=['9999999999', '999999-999', '99999x9999']), 'yes')
+
+    def test_num_checker_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            num_checker(nums=[])
+        self.assertEqual('Передан не список , или-список пуст', e.exception.args[0])
+
+    def test_str_cleaner_right(self):
+        self.assertEqual(str_cleaner(text_str='no more;fake,snd,lie,for me'),
+                         ['no', 'more', 'fake', 'snd', 'lie', 'for', 'me'])
+
+    def test_str_cleaner_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            str_cleaner(text_str='')
+        self.assertEqual('Передан неверный тип данных', e.exception.args[0])
+
+    def test_str_cleaner_spaces_right(self):
+        self.assertEqual(str_cleaner_spaces(text_str='no more;fake,snd,lie,for me'), 'no more fake snd lie for me')
+
+    def test_str_cleaner_spaces_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            str_cleaner_spaces(text_str='')
+        self.assertEqual('Передан неверный тип данных', e.exception.args[0])
+
+    def test_html_cleaner_right(self):
+        self.assertEqual(
+            html_cleaner(text_str='1NoahEmma2LiamOlivia3MasonSophia4JacobIsabella5WilliamAva6EthanMia7MichaelEmily'),
+            [('Noah', 'Emma'), ('Liam', 'Olivia'), ('Mason', 'Sophia'), ('Jacob', 'Isabella'), ('William', 'Ava'),
+             ('Ethan', 'Mia'), ('Michael', 'Emily')])
+
+    def test_html_cleaner_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            html_cleaner(text_str='')
+        self.assertEqual('Передан неверный тип данных', e.exception.args[0])
 
 
 if __name__ == '__main__':

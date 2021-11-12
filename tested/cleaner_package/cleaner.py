58ba8dc293d82efb2e1fa8e6837f186dadc13456
Philin-coder <alexey.user.1988@gmail.com>
2021-11-12 23:51:51 +0300

    Merge remote-tracking branch 'pyrepo/master'

---
+++ tested/cleaner_package/cleaner.py
@@ -0,0 +1,89 @@
+import re
+import os
+import wikipedia
+
+
+def cont_gen(title_str: str) -> str:
+    """
+    Генерация контента для записи в файл(загрузка статьи из wikipedia).
+    :param title_str: тема статьи.
+    :return: контент статьи.
+    Образец вызова
+    cont_gen(title_str='Python')
+    """
+    if isinstance(title_str, str) and title_str != '':
+        return str(wikipedia.page(title_str).content)
+    else:
+        raise TypeError('Передан неверный тип данных, или -пустая строка')
+
+
+def write_to_file(file_name: str, f_ext: str, f_data: str) -> str:
+    """
+    Запись данных в файл
+    :param file_name: иям файла.
+    :param f_ext: Расширение.
+    :param f_data: записываемые данные.
+    :return: Код состояния(успешна ли запись).
+    Образец вызова.
+    write_to_file(file_name='wiki2', f_ext='txt', f_data=cont_gen(title_str='Python'))
+    """
+    if isinstance(f_ext, str) and f_ext == 'txt':
+        with open(file_name + '.' + f_ext, 'w', encoding='utf-16') as fp:
+            fp.write(f_data)
+            fp.close()
+        stat_code = 'file_created'
+        return stat_code
+    else:
+        raise ValueError('Файл имеет неверный тип')
+
+
+def read_file(file_name: str, f_ext: str) -> str:
+    """
+    Чтение файла.
+    :param file_name:Имя файла
+    :param f_ext: Расширение.
+    :return: Прочитанные данные.
+    Образец вызова
+    read_file(file_name='wiki2', f_ext='txt')
+    """
+    if os.path.isfile(file_name + '.' + f_ext):
+        with open(file_name + '.' + f_ext, 'r', encoding='utf-16') as fp:
+            res = fp.readline()
+            fp.close()
+            return str(res)
+    else:
+        raise FileNotFoundError('нет такого файла')
+
+
+def searcher(work_str: str) -> str:
+    """
+    Обработка строки регулярными выражениями.
+    :param work_str: входная строка.
+    :return: результат обработки.
+    Образец вызова
+    searcher(work_str=read_file(file_name='wiki2', f_ext='txt'))
+    """
+    if isinstance(work_str, str) and work_str != '':
+        rx = re.sub("^(.+{}.+)$".format(re.escape(work_str.strip())), '', work_str, flags=re.MULTILINE)
+        return re.sub(r'\n\s*\n', '\n', rx, re.MULTILINE)
+    else:
+        raise TypeError('Передан неверный тип данных, или -пустая строка')
+
+
+def res_writer(file_name: str, f_ext: str, f_data: str) -> str:
+    """
+    Запись информации в файл.
+    :param file_name: выражениями файла.
+    :param f_ext: расширение
+    :param f_data: записываемые данные
+    :return: код состояния(успешна ли запись)
+    Образец вызова
+    res_writer(file_name='res', f_ext='txt', f_data=searcher(work_str=read_file(file_name='wiki2', f_ext='txt')))
+    """
+    if isinstance(f_ext, str) and f_ext == 'txt':
+        with open(file_name + '.' + f_ext, 'w', encoding='utf-16') as fp:
+            print(f_data, file=fp)
+            stat_code = 'file_created'
+            return stat_code
+    else:
+        raise ValueError('Файл имеет неверный тип')
---
+++ tests/reg_read_write_test.py
@@ -0,0 +1,71 @@
+from unittest import TestCase, main
+from tested.cleaner_package import cleaner
+from tested.cleaner_package.cleaner import cont_gen
+from tested.cleaner_package.cleaner import write_to_file
+from tested.cleaner_package.cleaner import read_file
+from tested.cleaner_package.cleaner import searcher
+from tested.cleaner_package.cleaner import res_writer
+import doctest
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(cleaner))
+    return tests
+
+
+class RegReadWriteTest(TestCase):
+    def test_cont_gen_is_str(self):
+        self.assertIsInstance(cont_gen(title_str='Python'), str)
+
+    def test_get_content_is_not_empty(self):
+        self.assertIsNotNone(cont_gen(title_str='Python'))
+
+    def test_alpha_and_nums(self):
+        self.assertTrue((any([i.isdigit() for i in cont_gen(title_str='Python')]) and (any(
+            [i.isalpha() for i in cont_gen(title_str='Python')]))), 'не содержит цифр и букв')
+
+    def test_get_content_wrong(self):
+        with self.assertRaises(TypeError) as e:
+            cont_gen(title_str='')
+        self.assertEqual('Передан неверный тип данных, или -пустая строка', e.exception.args[0])
+
+    def test_write_to_file_right(self):
+        self.assertEqual(write_to_file(file_name='wiki2', f_ext='txt', f_data=cont_gen(title_str='Python')),
+                         'file_created')
+
+    def test_write_to_file_wrong(self):
+        with self.assertRaises(ValueError) as e:
+            write_to_file(file_name='wiki2', f_ext='tt', f_data=cont_gen(title_str='Python'))
+        self.assertEqual('Файл имеет неверный тип', e.exception.args[0])
+
+    def test_file_reader_is_str(self):
+        self.assertIsInstance(read_file(file_name='wiki2', f_ext='txt'), str)
+
+    def test_file_reader_is_not_empty(self):
+        self.assertIsNotNone(read_file(file_name='wiki2', f_ext='txt'))
+
+    def test_file_reader_wrong(self):
+        with self.assertRaises(FileNotFoundError) as e:
+            read_file(file_name='wik', f_ext='txt')
+        self.assertEqual('нет такого файла', e.exception.args[0])
+
+    def test_searcher_is_str(self):
+        self.assertIsInstance(searcher(work_str=read_file(file_name='wiki2', f_ext='txt')), str)
+
+    def test_searcher_is_not_empty(self):
+        self.assertIsNotNone(searcher(work_str=read_file(file_name='wiki2', f_ext='txt')))
+
+    def test_res_writer_right(self):
+        self.assertEqual(res_writer(file_name='res', f_ext='txt',
+                                    f_data=searcher(work_str=read_file(file_name='wiki2', f_ext='txt'))),
+                         'file_created')
+
+    def test_res_writer_wrong(self):
+        with self.assertRaises(ValueError) as e:
+            res_writer(file_name='res', f_ext='123',
+                       f_data=searcher(work_str=read_file(file_name='wiki2', f_ext='txt')))
+        self.assertEqual('Файл имеет неверный тип', e.exception.args[0])
+
+
+if __name__ == '__main__':
+    main()

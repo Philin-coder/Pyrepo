ad3852d75e931b58637cfb5cfd9f1363986110f4
Philin-coder <alexey.user.1988@gmail.com>
2021-11-15 15:47:50 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- forum_task_package/several/Классс на python 3.py
+++
@@ -1,9 +0,0 @@
-class first:
-    def mf(self):
-        print('hello')
-        for i in (dir(first)):
-            print(i)
-
-
-f = first()  # объект
-f.mf()  # вызов метода
--- tested/method_explorer_package/method_explorer_mod.py
+++ tested/method_explorer_package/method_explorer_mod.py
@@ -1,10 +1,20 @@
 class First(object):
     @staticmethod
-    def my_func(test_str) -> str:
+    def my_exm_method(test_str: str) -> str:
         """
         Тестовый метод (искомый).
         :param test_str: текстовая строка, выводимая методом.
         :return: то, что выводит метод.
+        >>> Fr = First()
+        >>> Fr.my_exm_method(test_str='hello')
+        'hello'
+        >>> Fr.class_method_viewer(cl_name=First)
+        ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'class_method_finder', 'class_method_viewer', 'my_exm_method']
+        >>> Fr.class_method_finder(dir_list=Fr.class_method_viewer(cl_name=First), meth_name='my_exm_method')
+        'Метод my_exm_method найден'
+        >>> Fr.class_method_finder(dir_list=Fr.class_method_viewer(cl_name=First), meth_name='y_exm_method')
+        'Метод y_exm_method  не найден'
+
         """
         if isinstance(test_str, str):
             return test_str
@@ -18,7 +28,7 @@ class First(object):
         :param cl_name: имя класса.
         :return: список методов заданного класса.
         """
-        if isinstance(cl_name, object) and cl_name.__name__ == 'First':
+        if cl_name is not None and __class__ == First:
             return dir(cl_name)
         else:
             raise TypeError('передан неверный тип данных')
@@ -38,11 +48,3 @@ class First(object):
                 return f'Метод {meth_name}  не найден'
         else:
             raise TypeError('передан неверный тип данных')
-
-
-if __name__ == '__main__':
-    Fr = First()
-    print(Fr.my_func(test_str='hello'))
-    print(Fr.class_method_viewer(cl_name=First))
-    print(Fr.class_method_finder(dir_list=Fr.class_method_viewer(cl_name=First), meth_name='my_func'))
-    print(Fr.class_method_finder(dir_list=Fr.class_method_viewer(cl_name=First), meth_name='y_func'))

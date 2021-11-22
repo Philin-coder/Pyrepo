775adef6db7f3f10e9e9e6dd063c08a669ae56ce
Philin-coder <alexey.user.1988@gmail.com>
2021-11-22 15:40:23 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- tests/range_filter_test.py
+++ tests/range_filter_test.py
@@ -9,7 +9,7 @@ def load_tests(loader, tests, ignore):
     return tests
 
 
-class MyTestCase(TestCase):
+class MyLGenTestCase(TestCase):
     def test_int_in_input(self):
         self.assertEqual(another_mas_gen(n=80),
                          [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,

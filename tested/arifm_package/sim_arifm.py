fa66f7ec532c77cf3e8197c98118f2e705c42bd6
Philin-coder <alexey.user.1988@gmail.com>
2021-11-23 13:44:07 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- tested/arifm_package/sim_arifm.py
+++ tested/arifm_package/sim_arifm.py
@@ -7,9 +7,9 @@ def perv(a: int) -> float:
     :param a: число
     :return: первообразная от него
     >>> perv(a=12)
-     8.71673
+    8.71673508601987
     >>> perv(a=7)
-    1.76643
+    1.766437127533977
     """
     if isinstance(a, int):
         return 0.5 * a - 0.25 * a * math.sin(2 * a)
@@ -24,10 +24,9 @@ def suber(x: float, y: float) -> [float, str]:
     :param y: второе.
     :return: результат.
     >>> suber(x=perv(a=12), y=perv(a=1))
-    'точное значение интеграла 8.44406'
+    'точное значение интеграла 8.44405944272629'
     """
     if isinstance(x, float) and isinstance(y, float):
-        res = x - y
-        return f'точное значение интеграла {res:.5f}'
+        return f'точное значение интеграла {x - y}'
     else:
         raise TypeError('Передан неверный тип данных')

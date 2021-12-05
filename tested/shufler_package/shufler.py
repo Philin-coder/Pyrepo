7ede0a6d46e1654bda0c5a16a35344717c7ac45a
Philin-coder <alexey.user.1988@gmail.com>
2021-12-05 23:48:32 +0300

    Merge remote-tracking branch 'pyrepo/master'

--- tested/shufler_package/shufler.py
+++ tested/shufler_package/shufler.py
@@ -5,7 +5,7 @@ def randomer(n: int) -> list:
     """
     Эмуляция лотереи 6 из 45.
     :param n: диапазон.
-    :return: список чисел  без повторов
+    :return: список чисел без повторов.
     """
     if isinstance(n, int) and n > 0:
         ints = [random.randint(5, n + 1) for _ in range(n)]

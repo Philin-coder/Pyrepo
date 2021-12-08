4925c226b252e4ab9721de091171ff8d91731f19
Philin-coder <alexey.user.1988@gmail.com>
2021-12-08 09:51:37 +0300

    Merge remote-tracking branch 'pyrepo/master'

---
+++ guess_num_package/num_gesser.py
@@ -0,0 +1,29 @@
+from random import randint
+
+
+def guess_number(var2: int) -> str:
+    """
+    Игра угадай число.
+    :param var2:загаданное число.
+    :return: Строка, сообщение пользователю о том, какое число за сколько попыток угадали.
+    Образец вызова
+    """
+    if isinstance(var2, int):
+        attempts = 1
+        var1 = randint(1, 100)
+        print("Загадано число от 1 до 99")
+        while var1 != var2:
+            if var1 > var2:
+                print(f"Угадываемое число больше {var2} ")
+            elif var1 < var2:
+                print(f"Угадываемое число меньше {var2} ")
+            attempts += 1
+            var2 = int(input("Ваш вариант? - "))
+        return f'Вы угадали число {var1} за {attempts} попыток '
+    else:
+        raise TypeError('передан неверный тип данных')
+
+
+if __name__ == '__main__':
+    # print(guess_number(var2=78))
+    print(guess_number(var2='78'))

7ca1a4fe663efd81c8ec4dd0cfa33b18389d156f
Alexey <80147483+Philin-coder@users.noreply.github.com>
2024-08-04 11:28:23 +0300

    models

--- django_stip/stip/stip_app/views.py
+++ django_stip/stip/stip_app/views.py
@@ -9,6 +9,21 @@ from pip._vendor.requests.api import request
 from django.core.serializers.json import DjangoJSONEncoder
 from datetime import datetime
 from .forms import UserForm
+from .models import Person
+
+people=Person.objects.all()
+print(people.query)
+
+people=people.filter(name="Tom")
+print(people.query)
+
+people=people.filter(age=31)
+print(people.query)
+
+
+for person in people:
+    print(f"{person.id}.{person.name}-{person.age}")
+
 
 
 def index(request):

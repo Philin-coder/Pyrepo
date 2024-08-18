9e8a20f1026f1efe8937ed1f68c30b80bb896583
Alexey <80147483+Philin-coder@users.noreply.github.com>
2024-08-18 21:59:56 +0300

    hyper_clean

--- forum_task_package/Нажатие клавиши.py
+++
@@ -1,4 +0,0 @@
-import win32api
-import win32con
-
-win32api.keybd_event(win32con.SHIFT_PRESSED, 0, win32con.KEYEVENTF_EXTENDEDKEY, 0)
--- tested/another_random_package/randomer.py
+++
@@ -1,7 +0,0 @@
-import random
-
-
-def my_rand(start_p: int, end_p: int, ) -> int:
-    if isinstance(start_p, int) and isinstance(end_p, int):
-        return random.randint(start_p, end_p)
-    raise TypeError('Введите  целые числа')
